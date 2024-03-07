57effc36254cdf57b8b35adca9de68c6a7a12ff8
DenisGitHubs <128564592+DenisGitHubs@users.noreply.github.com>
2024-03-07 21:24:21 +0700

    1

--- src/Components/ProductList/list.jsx
+++ src/Components/ProductList/list.jsx
@@ -1,7 +1,7 @@
 import './list.css'
 import { Pagination } from "../Pagination/pagination";
 import { sendRequest } from "../../API/api"
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { ProductItem } from '../ProductItem/item';
 import { FilterMenu } from '../Filter/filter';
 import { editAllId, getNames, getNewSet, removeDuplicates, saveCombinedData, saveIDFilter } from '../../Helpers/helpers';
@@ -17,16 +17,28 @@ export const ProductList = () => {
   const {checkPage , currentPage }= useSelector(state => state.page)
   const filter = useSelector(state => state.filter.filter)
 
+// ф-я получения данных, если данных для текущей страницы меньше 50 после удаления дублей
+const secondAdd = useCallback(async (offset, limit, addOffset) => {
+  const response = await sendRequest('get_ids', { offset: offset, limit: limit });
+  if (response && data.length < 50) {
+    const newData = removeDuplicates(response.result, allReceivedIDs);
+    if(newData.length === 0){
+      secondAdd(addOffset + 1, limit, addOffset);
+      return
+    }
+    saveCombinedData(setData, response.result)
+    editAllId(response.result, setAllReceivedIDs)
+  } else {
+    secondAdd(offset, limit);
+  }
+}, [data, allReceivedIDs, setData]);
+
   // ф-я получения Id для текущей страницы
-  const onPageChangeNext = async (page) => {
+  const onPageChangeNext = useCallback(async (page) => {
     const offset = page * 50 - 50;
     if(filter === null) {
-      console.log(allReceivedIDs.length);
-      console.log()
-      console.log('use');
       const response = await sendRequest('get_ids', { offset: offset, limit: 50 });
       if (response && allReceivedIDs.length > 0) {
-        console.log('use2');
         setData(prevState => {
           let newData = prevState;
           newData = removeDuplicates(response.result, allReceivedIDs);
@@ -34,22 +46,20 @@ export const ProductList = () => {
         })
         editAllId(response.result, setAllReceivedIDs)
       } else if (response && allReceivedIDs.length === 0){
-        console.log('use4');
         setData(getNewSet(response.result));
         editAllId(response.result, setAllReceivedIDs)
       } else {
         onPageChangeNext(page)
       }
     }
-  };
+  }, [filter, allReceivedIDs, setData ]); 
 
 // первый запрос для получения данных с API
   useEffect(() => {
     if (currentPage === 1 && data.length === 0 && filter === null) {
       onPageChangeNext(currentPage)
-      return
     }
-  }, [currentPage, data, filter ])
+  }, [currentPage, data, filter, onPageChangeNext ])
 
 // очистка поля и получение подробных данных о товарах и внесение их в стейт для вывода.
   useEffect(() => {
@@ -83,7 +93,7 @@ export const ProductList = () => {
       const addOffset = offset + data.length
       secondAdd(offset, limit, addOffset)
     }
-  }, [data, currentPage, allReceivedIDs, filter,])
+  }, [data, currentPage, allReceivedIDs, filter, secondAdd])
 
   // при нажатии кнопки назад, мы не делаем запросы снова, а используем полученные ранее ID.
   useEffect(() => {
@@ -100,21 +110,7 @@ export const ProductList = () => {
     getNames(getNewSet(data), setItems)
     }
   }, [data, filter])
-// ф-я получения данных, если данных для текущей страницы меньше 50 после удаления дублей
-  const secondAdd = async (offset, limit, addOffset) => {
-    const response = await sendRequest('get_ids', { offset: offset, limit: limit });
-    if (response && data.length < 50) {
-      const newData = removeDuplicates(response.result, allReceivedIDs);
-      if(newData.length === 0){
-        secondAdd(addOffset + 1, limit, addOffset);
-        return
-      }
-      saveCombinedData(setData, response.result)
-      editAllId(response.result, setAllReceivedIDs)
-    } else {
-      secondAdd(offset, limit);
-    }
-  };
+
 
 // ф-я для повторного запроса для фильтра "product" т.к. API чувствителен к регистру. здесь делаем заглавной первую букву первого слова
  const secondFilterSearch = async (requestValue) => {
